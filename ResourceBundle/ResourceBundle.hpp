cdfd115b8543ed834cc564be5c9e6495b1d73ba7
Alec Iverson <adiverso93@gmail.com>
2015-09-22 00:40:31 -0700

    Clarification in comments

--- ResourceBundle/ResourceBundle.hpp
+++ ResourceBundle/ResourceBundle.hpp
@@ -11,23 +11,32 @@ ResourceBundle files are in a binary format:
 	Header:
 		4 bytes: Number of resources
 
-			List of each resource, with name, and byte offset and length:
-				4 bytes: name length
-				<name length> bytes: name
-				4 bytes: byte offset
-				4 bytes: byte length
+		List of each resource, with name, and byte offset and length:
+			4 bytes: name length
+			<name length> bytes: name
+			4 bytes: byte offset (from beginning of file)
+			4 bytes: byte length
 
-		Resource data in bytes:
+	Resource data:
 */
 
+/*
+Class holding an open file handle to a resource file. Data is only extracted upon request, ie:
+The whole file is not read and stored in memory.
+A call to ResourceBundle::get reads the bytes from the file and returns a pointer to said bytes.
+A call to the same resource name WILL read the file again.
+*/
 class ResourceBundle
 {
 	public:
+		// debating about unique_ptr now... Works fine for APIs that end up copying the memory anyway.
+		// but some APIs may not copy. Unless they're already set up to use smart pointers, this causes an issue.
 		using Resource = std::pair<std::unique_ptr<char[]>, unsigned int>;
 
 		ResourceBundle() = default;
 		~ResourceBundle() = default;
 
+		// Non-copyable
 		ResourceBundle(const ResourceBundle&) = delete;
 
 		bool openFile(const std::string& file);
